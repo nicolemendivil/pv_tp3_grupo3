3eb82c9e056fbcc8580493c2665f30341dbd9786
debgiss98 <deborachavez1998@gmail.com>
2026-06-17 16:52:14 -0300

    parte 5: edición de perfil y persistencia con LocalStorage

--- pv_tp3_grupo3/src/context/UsuarioContext.jsx
+++ pv_tp3_grupo3/src/context/UsuarioContext.jsx
@@ -1,15 +1,33 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState,useEffect } from "react";
 const UsuarioContext = createContext();
 export const UsuarioProvider = ({ children }) => {
-  const [usuario, setUsuario] = useState({
+  const [usuario, setUsuario] = useState(()=> {
+    const usuarioGuardado=localStorage.getItem("usuario");
+    let usuarioInicial;
+    if (usuarioGuardado){
+      usuarioInicial=JSON.parse(usuarioGuardado);
+    } else{
+      usuarioInicial={   
       nombre: "Nicole",
       dni: "1234",
       rol: "Alumno",
       institucion: "Universidad Nacional de Jujuy",
-    });
+    };
+  }
+  return usuarioInicial;
+});
+
   const actualizarPerfil = (datosActualizados) => {
     setUsuario(datosActualizados);
   };
+   useEffect(() => {
+
+    localStorage.setItem(
+      "usuario",
+      JSON.stringify(usuario)
+    );
+
+  }, [usuario]);
 
   return (
     <UsuarioContext.Provider value={{ usuario, actualizarPerfil }}>
--- pv_tp3_grupo3/src/views/PerfilUsuario.jsx
+++ pv_tp3_grupo3/src/views/PerfilUsuario.jsx
@@ -5,11 +5,31 @@ import {
   List,
   ListItem,
   ListItemText,
+  Button,
+  TextField,
 } from "@mui/material";
 import { useUsuario } from "../context/UsuarioContext";
+import { useState } from "react";
 
 const PerfilUsuario = () => {
-  const { usuario } = useUsuario();
+  //const { usuario } = useUsuario();
+  const { usuario, actualizarPerfil } = useUsuario();
+  // Estado para habilitar/deshabilitar edición
+  const [editar, setEditar] = useState(false);
+  // Estado del formulario
+  const [formulario, setFormulario] = useState(usuario);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormulario({
+      ...formulario,
+      [name]: value
+    });
+  };
+  const guardarCambios = () => {
+    actualizarPerfil(formulario);
+    setEditar(false);
+  };
+  if(!editar){
   return (
     <Container>
       <Typography variant="h4" gutterBottom>
@@ -34,9 +54,73 @@ const PerfilUsuario = () => {
             />
           </ListItem>
         </List>
+         <Button
+              variant="contained"
+              onClick={() => setEditar(true)}
+            >
+              Editar Perfil
+          </Button>
       </Paper>
     </Container>
   );
 };
+return (
+  <Container>
+    <Typography variant="h4" gutterBottom>
+      Editar Perfil
+    </Typography>
+    <Paper sx={{ p: 2 }}>
+      <TextField
+        fullWidth
+        margin="normal"
+        label="Nombre"
+        name="nombre"
+        value={formulario.nombre}
+        onChange={handleChange}
+      />
+      <TextField
+       fullWidth
+       margin="normal"
+       label="DNI"
+       name="dni"
+       value={formulario.dni}
+       onChange={handleChange}
+      />
 
-export default PerfilUsuario;
+      <TextField
+       fullWidth
+       margin="normal"
+       label="Rol"
+       name="rol"
+       value={formulario.rol}
+       onChange={handleChange}
+      />
+
+      <TextField
+        fullWidth
+        margin="normal"
+        label="Institución"
+        name="institucion"
+        value={formulario.institucion}
+        onChange={handleChange}
+      />
+      <Button
+        variant="contained"
+        onClick={guardarCambios}
+      >
+        Guardar Cambios
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          setFormulario(usuario);
+          setEditar(false);
+        }}
+      >
+        Cancelar
+      </Button>
+    </Paper>
+  </Container>
+);
+};
+export default PerfilUsuario; 
